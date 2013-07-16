3dfad2a834dd601571bc42b21f909e27797433fc
Tymon Tobolski <i@teamon.eu>
2013-07-16 19:54:26 +0200

    rails 4 app init

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.0'
+
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+
+# Use CoffeeScript for .js.coffee assets and views
+gem 'coffee-rails', '~> 4.0.0'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAngularKarmaExample::Application.config.secret_key_base = '8443010f444c173cf8b0aca86d4481c7cb13c0111131e110122398eebf49a4e6ee5214539c461446b9aad387acf491975a7f573355211f4eac5d3739410c7fe5'
