33142b67bc0ce3d210e43e5e88dbf1b635ab7c6a
Snuff <ggg1321@gmail.com>
2013-10-13 01:44:15 +0400

    fix karma config for version 0.10.1

--- lib/karma.rb
+++ lib/karma.rb
@@ -4,40 +4,46 @@ module Karma
       confjs = File.join(dir, "karma.conf.js")
 
       adapter = case opts[:adapter]
-      when :angular then "ANGULAR_SCENARIO"
-      when :jasmine then "JASMINE"
+      when :angular then "ng-scenario"
+      when :jasmine then "jasmine"
       end
 
       files = <<-EOS
         [
-          #{adapter},
-          #{adapter}_ADAPTER,
           #{opts[:files].map(&:dump).join(",\n")}
         ]
       EOS
 
       proxies = if opts[:proxy]
-        "proxies = #{opts[:proxy].to_json};"
+        "proxies : #{opts[:proxy].to_json},"
       else
         ""
       end
 
       File.open(confjs, "w") do |f|
         f.write <<-EOS
-          basePath = '/'; // this is important - without this coffee files form gems will not be processed
-          files = #{files};
-          exclude = [];
-          reporters = ['progress'];
-          port = 9876;
-          runnerPort = 9100;
-          colors = true;
-          logLevel = LOG_INFO;
-          autoWatch = #{!opts[:single_run]};;
-          browsers = ['Chrome'] // or 'Firefox', 'Safari', 'PhantomJS'
-          captureTimeout = 60000;
-          singleRun = #{!!opts[:single_run]};
-          #{proxies}
-          urlRoot = '/__karma__/';
+          module.exports = function (config) {
+            config.set({
+              basePath : '/',
+
+              // Fix for "JASMINE is not supported anymore" warning
+              frameworks : ["#{adapter}"],
+
+              files : #{files},
+              exclude : [],
+              autoWatch : #{!opts[:single_run]},
+              browsers : ['Chrome'],
+              singleRun : #{!!opts[:single_run]},
+              reporters : ['progress'],
+              port : 9876,
+              runnerPort : 9100,
+              colors : true,
+              logLevel : LOG_INFO,
+              #{proxies}
+              urlRoot : '/__karma__/',
+              captureTimeout : 60000
+            });
+          }
         EOS
       end
 
